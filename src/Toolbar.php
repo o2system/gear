a5d308ba6f2fe7e7d2ef398aab71860b57711647
Steeve Andrian Salim <steevenz@ymail.com>
2018-04-25 21:20:02 +0700

    add database feature

--- src/Toolbar.php
+++ src/Toolbar.php
@@ -63,9 +63,9 @@ class Toolbar
         $metrics = array_reverse( $metrics );
 
         $files = $this->getFiles();
+        $queries = $this->getQueries();
         $logs = $this->getLogs();
         $vars = $this->getVars();
-        $database = $this->getDatabase();
 
         ob_start();
         include __DIR__ . '/Views/Toolbar.php';
@@ -103,19 +103,9 @@ class Toolbar
 
     // ------------------------------------------------------------------------
 
-    public function getDatabase()
+    public function getQueries()
     {
-        $database = [];
-
-        if(class_exists('O2System\Framework', false)) {
-            $connections = database()->getIterator();
-
-            foreach($connections as $offset => $connection) {
-                $database[ $offset ] = $connection->getQueries();
-            }
-        }
-
-        return $database;
+        return [];
     }
 
     /**
